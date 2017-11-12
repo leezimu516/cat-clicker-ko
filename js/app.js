c098306be66e52e3493c7bf3246c089a90365579
kai li <adamlee516@gmail.com>
2017-11-12 18:59:33 -0500

    multiple cat, cat list in html

--- js/app.js
+++ js/app.js
@@ -1,18 +1,66 @@
+var initialCats = [
+    {
+        clickCount : 0,
+        name : 'Tabby',
+        imgSrc : 'img/434164568_fea0ad4013_z.jpg',
+        imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568',
+        nickname: ["kk", "kiki", "lili"]
+    },
+    {
+        clickCount : 0,
+        name : 'Tiger',
+        imgSrc : 'img/4154543904_6e2428c421_z.jpg',
+        imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904',
+        nickname: ["kk", "kiki", "lili"]
+    },
+    {
+        clickCount : 0,
+        name : 'Scaredy',
+        imgSrc : 'img/22252709_010df3379e_z.jpg',
+        imgAttribution : 'https://www.flickr.com/photos/kpjas/22252709',
+        nickname: ["kk", "kiki", "lili"]
+    },
+    {
+        clickCount : 0,
+        name : 'Shadow',
+        imgSrc : 'img/1413379559_412a540d29_z.jpg',
+        imgAttribution : 'https://www.flickr.com/photos/malfet/1413379559',
+        nickname: ["kk", "kiki", "lili"]
+    },
+    {
+        clickCount : 0,
+        name : 'Sleepy',
+        imgSrc : 'img/9648464288_2516b35537_z.jpg',
+        imgAttribution : 'https://www.flickr.com/photos/onesharp/9648464288',
+        nickname: ["kk", "kiki", "lili"]
+    }
+];
+
 var ViewModel = function() {
-    this.currentCat = ko.observable(new Cat());
+    var self = this;
+
+    this.catList = ko.observableArray([]);
+
+    initialCats.forEach(function(catItem) {
+        self.catList.push(new Cat(catItem));
+    });
+    this.currentCat = ko.observable(this.catList()[0]);
 
     this.incrementCounter = function() {
         this.clickCount(this.clickCount() + 1);
-        
-        
+    };
+
+    this.changeCat = function(clickedCat) {
+        self.currentCat(clickedCat)
+        // console.log("hi");
     };
 }
 
-var Cat = function() {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('tabby');
-    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-    this.imgAttribution = ko.observable('flicker');
+var Cat = function(data) {
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.imgAttribution = ko.observable(data.imgAttribution);
     
     //cat level using clickcount
     this.level = ko.computed(function() {
@@ -30,11 +78,7 @@ var Cat = function() {
         return level;
     }, this);
 
-    this.nickname = ko.observableArray([
-        {catname:'ki'}, 
-        {catname:'kk'}, 
-        {catname:'kki'}
-    ]);
+    this.nickname = ko.observableArray(data.nickname);
 }
 
 ko.applyBindings(new ViewModel());
